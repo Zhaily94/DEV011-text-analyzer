799a5fe6415679c729b9c1ba1e16ba25e14187f8
zhaira <zhailysweetly@gmail.com>
2023-08-23 18:16:11 -0600

    funcionan todos los metodos menos getAverageWordLength y getWordCount pendiente

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,70 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+   
+    if (text === ''){
+      return 0;
+    }
+    // s.replace(/\s+/g, ' ').trim();
+
+    const s = text.replace(/^\s+/, '');
+    const t = s.replace(/[a-zA-Z]/g, '');
+    const n = t.replace(/[0-9]/g, '');
+
+    return n.length;
+  
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    if (text === ''){
+      return 0;
+    }
+    return text.length;
+
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    if (text === ''){
+      return 0;
+    }
+    const txtSpace = text.replace(/\s/g, '');
+    const txtSigns = txtSpace.replace(/[\W_]/g, '');
+    return txtSigns.length;
+    
+  
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    if (text === '') {
+      return 0;
+    } 
+    const txtSpace = text.replace(/\s/g, '');
+    const txtSigns = txtSpace.replace(/[\W_]/g, '');
+    const n = txtSigns.replace(/[a-zA-Z]/g, '');
+    return n.length;
+    
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+
+    if (text === '') {
+      return 0;
+    }
+
+    let suma = 0;
+    const txtSpace = text.replace(/\s/g, '');
+    const txtSigns = txtSpace.replace(/[\W_]/g, '');
+    const txtLetters = txtSigns.replace(/[a-zA-Z]/g, '');
+
+    for (let i = 0; i <= txtLetters.length; i++) {
+      const iw = parseInt(txtLetters[i]); 
+      if (iw > 0) {
+        suma += iw;
+      }
+    } return suma;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+document.addEventListener('keyup', function (event) {
+  let caracterCount = 0;
+  let caracterCount2 = 0;
+  let caracterCountNumbers = 0;
+  let countWords = 0;
+  let sumNumbers = 0;
+
+  let texto = '';
+  // let numero = '';
+
+  if (event.key) {
+    texto = document.getElementById("text-area").value; // En esta parte obtenemos los valores del input (text-area) y los guardamos en una variable text
+    // numero = parseInt(texto);
+
+    caracterCount = analyzer.getCharacterCount(texto); // llamamos al metodo, le otorgamos un parametro y lo guardamos en la variable car
+    caracterCount2 = analyzer.getCharacterCountExcludingSpaces(texto);
+    caracterCountNumbers = analyzer.getNumberCount(texto);
+    countWords = analyzer.getWordCount(texto);
+    sumNumbers = analyzer.getNumberSum(texto);
+
+
+    document.querySelector('.character-count').innerHTML = "Caracteres: " + caracterCount; // solo mandamos el dato que nos mando el metodo y lo mostramos en pantalla mediante la clase
+    document.querySelector('.character-no-spaces-count').innerHTML = "Caracteres sin espacios: " + caracterCount2;
+    document.querySelector('.number-count').innerHTML = "Numeros: " + caracterCountNumbers;
+    document.querySelector('.word-count').innerHTML = "Palabras: " + countWords;
+    document.querySelector('.cnumber-sum').innerHTML = "Suma de numeros: " + sumNumbers;
+
+  }
+});
