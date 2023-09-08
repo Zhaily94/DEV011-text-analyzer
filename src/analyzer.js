1af66c05f7fe28601ce09957eb19aac6c5841337
zhaira <zhailysweetly@gmail.com>
2023-09-08 12:02:18 -0600

    ajustes funciones estilos y html

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,24 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-   
-    if (text === ''){
+
+    if (text === '') {
       return 0;
     }
-    // s.replace(/\s+/g, ' ').trim();
+   
+    // const s = text.replace(/^\s+/, ''); 
+    const s = text.trim();// quita los espacios de el inicio y al final
+    const t = s.replace(/[a-zA-Z]/g, ''); // quita las letras
+    const n = t.replace(/[0-9]/g, ''); // quita los numeros
+    const txtSigns = n.replace(/,/g, ''); // quita las comas
+    
 
-    const s = text.replace(/^\s+/, '');
-    const t = s.replace(/[a-zA-Z]/g, '');
-    const n = t.replace(/[0-9]/g, '');
+    return txtSigns.length;
 
-    return n.length;
-  
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    if (text === ''){
+    if (text === '') {
       return 0;
     }
     return text.length;
@@ -24,48 +26,103 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-    if (text === ''){
+    if (text === '') {
       return 0;
     }
     const txtSpace = text.replace(/\s/g, '');
     const txtSigns = txtSpace.replace(/[\W_]/g, '');
     return txtSigns.length;
-    
-  
+
+
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-    if (text === '') {
+    const signos = /,|;|:|"|«|»|¡|¿|!|&+/g;
+    const signosEspeciales = /\u0027|\u002D|\0022|\.|\{|\}|\(|\)|\[|\]|\?|\s|\n|\t|\\|\r|\b|\f|\v+/g;
+    const patternLetters = /[a-zA-Z](\d+)/g; // patron para saaber si hay letras adelante y destras de un numero
+    const pattern = /\d+\.\d+/g;
+    const p = /\d+/g;
+
+    if(text.match(patternLetters)){
       return 0;
-    } 
-    const txtSpace = text.replace(/\s/g, '');
-    const txtSigns = txtSpace.replace(/[\W_]/g, '');
-    const n = txtSigns.replace(/[a-zA-Z]/g, '');
-    return n.length;
-    
+    }
+
+    if (text === '') { // valida que text no venga vacio
+      return 0;
+    }
+  
+    if (text.match(pattern)) {
+      return text.match(pattern).length;
+    }
+    if (text.match(p)) {
+      return text.match(p).length;
+    }
+
+    if (text.match(signos) || text.match(signosEspeciales)) {
+      return 0;
+    }
   },
+
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const patternLetters = /[a-zA-Z](\d+)/g; // patron para saaber si hay letras adelante y destras de un numero
+    if(text.match(patternLetters)){
+      return 0;
+    }
 
     if (text === '') {
       return 0;
     }
 
-    let suma = 0;
-    const txtSpace = text.replace(/\s/g, '');
-    const txtSigns = txtSpace.replace(/[\W_]/g, '');
-    const txtLetters = txtSigns.replace(/[a-zA-Z]/g, '');
 
-    for (let i = 0; i <= txtLetters.length; i++) {
-      const iw = parseInt(txtLetters[i]); 
-      if (iw > 0) {
-        suma += iw;
-      }
-    } return suma;
+    const pattern = /\d+\.\d+/g;
+    const txtSpace = text.replace(/\s/g, '');
+    let txtLetters = txtSpace.replace(/[a-zA-Z]/g, '');
+    let suma = 0;
+    if (txtLetters.match(pattern)) {
+      const numF = txtLetters.match(pattern);
+      for (let i = 0; i <= numF.length; i++) {
+        const iw = parseFloat(numF[i]);
+        if (iw > 0.0) {
+          suma += iw;
+        }
+      } return suma;
+    }
+    else {
+      // eslint-disable-next-line no-const-assign
+      txtLetters = txtLetters.replace(/[\W_]/g, '');
+      for (let i = 0; i <= txtLetters.length; i++) {
+        const iw = parseFloat(txtLetters[i]);
+        if (iw > 0.0) {
+          suma += iw;
+        }
+      } return suma;
+    }
+  },
+
+  getAverageWordLength: (text) => {
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    if (text === '') {
+      return 0;
+    }
+    // text = text.split(' ').length
+
+    const palabras = parseFloat(analyzer.getWordCount(text));
+
+    // const caracteres = parseFloat(analyzer.getCharacterCount(text));
+    const caracteres = text.replace(/\s/g, '');
+    // const caracteres = parseFloat(analyzer.getCharacterCountExcludingSpaces(text));
+    const promedio = caracteres.length / palabras;
+    
+  
+    return parseFloat(promedio.toFixed(2));
+
+
   },
 };
+
+
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
+
+document.addEventListener("click", function() {
+  document.getElementById("text-area").value = "";
+  document.querySelector('.character-count').innerHTML = "Caracteres: " + 0;
+  document.querySelector('.character-no-spaces-count').innerHTML = "Caracteres sin espacios: " + 0;
+  document.querySelector('.number-count').innerHTML = "Numeros: " + 0;
+  document.querySelector('.word-count').innerHTML = "Palabras: " + 0;
+  document.querySelector('.cnumber-sum').innerHTML = "Suma de numeros: " + 0;
+  document.querySelector('.word-length-average').innerHTML = "Caracteres promedio: " + 0;
+
+});
+
 document.addEventListener('keyup', function (event) {
   let caracterCount = 0;
   let caracterCount2 = 0;
   let caracterCountNumbers = 0;
   let countWords = 0;
   let sumNumbers = 0;
+  let promWords = 0;
 
   let texto = '';
   // let numero = '';
@@ -21,6 +34,9 @@ document.addEventListener('keyup', function (event) {
     caracterCountNumbers = analyzer.getNumberCount(texto);
     countWords = analyzer.getWordCount(texto);
     sumNumbers = analyzer.getNumberSum(texto);
+    promWords = analyzer.getAverageWordLength(texto);
+    // const c = parseInt(caracterCount2);
+    // const p = parseInt(countWords);
 
 
     document.querySelector('.character-count').innerHTML = "Caracteres: " + caracterCount; // solo mandamos el dato que nos mando el metodo y lo mostramos en pantalla mediante la clase
@@ -28,6 +44,11 @@ document.addEventListener('keyup', function (event) {
     document.querySelector('.number-count').innerHTML = "Numeros: " + caracterCountNumbers;
     document.querySelector('.word-count').innerHTML = "Palabras: " + countWords;
     document.querySelector('.cnumber-sum').innerHTML = "Suma de numeros: " + sumNumbers;
+    document.querySelector('.word-length-average').innerHTML = "Caracteres promedio: " + promWords;
 
   }
+
+  
 });
+
+
